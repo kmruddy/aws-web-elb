da9441f0ffbb509634cb11b6475134ab63b185d9
Kyle Ruddy <kmruddy@users.noreply.github.com>
2020-09-25 13:55:34 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-west-2"
+}
+
+variable "name" {
+  description = "Desired name for each ELB"
+  default     = "Demo"
+}
+
+variable "subnet_cidr" {
+  description = "Desired default subnet CIDR for the instances to live"
+  default     = "10.0.1.0/24"
+}
