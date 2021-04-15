c03c6108b61fd54e09d181ee0598f0e920d7a0d3
Kyle Ruddy <kmruddy@users.noreply.github.com>
2021-04-15 19:03:42 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "elb_name" {
   description = "ELB Name"
-  value       = module.elb-approved[0].aws_elb.web.name
+  value       = module.elb-approved.aws_elb.web.name
 }
 
 output "elb_sg" {
   description = "ELB Security Group Name"
-  value       = module.elb-approved[0].aws_elb.web.source_security_group
+  value       = module.elb-approved.aws_elb.web.source_security_group
 }
