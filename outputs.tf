cbd89294d0ae4f0f9dfaecb4a97d62f680531c53
Kyle Ruddy <kmruddy@users.noreply.github.com>
2021-04-15 19:05:11 -0400

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "elb_name" {
-  description = "ELB Name"
-  value       = module.elb-approved.aws_elb.web.name
-}
-
-output "elb_sg" {
-  description = "ELB Security Group Name"
-  value       = module.elb-approved.aws_elb.web.source_security_group
-}
